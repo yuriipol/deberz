0e7cf18136a45bbd9f9c4b73e8874b9fa6664707
yuriipol <104125040+yuriipol@users.noreply.github.com>
2023-05-08 14:58:12 +0300

    add navigate to home page

--- src/Pages/AuthPage/LoginPage/LoginPage.jsx
+++ src/Pages/AuthPage/LoginPage/LoginPage.jsx
@@ -1,10 +1,10 @@
 import { useDispatch } from "react-redux";
 import { loginOperation } from "../../../Redux/auth/auth-operations";
-// import { Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import LoginForm from "../../../Component/LoginForm/LoginForm";
 // import { auth } from "../../../Shared/firebase/config";
 // import { onAuthStateChanged } from "firebase/auth";
-// import useAuth from "../../Shared/hooks/useAuth";
+import useAuth from "../../../Shared/hooks/useAuth";
 
 const LoginPage = () => {
   ///* проверка пользователя в firebase (online или нет) */
@@ -18,10 +18,10 @@ const LoginPage = () => {
   const onLogin = (data) => {
     dispatch(loginOperation(data));
   };
-  // const isLogin = useAuth();
-  // if (isLogin) {
-  //   return <Navigate to="/home" />;
-  // }
+  const isLogin = useAuth();
+  if (isLogin) {
+    return <Navigate to="/home" />;
+  }
   return (
     <div className="container">
       <LoginForm onSubmit={onLogin} />
--- src/Pages/AuthPage/RegisterPage/RegisterPage.jsx
+++ src/Pages/AuthPage/RegisterPage/RegisterPage.jsx
@@ -1,20 +1,18 @@
 import RegisterForm from "../../../Component/RegisterForm/RegisterForm";
-// import { singUp } from "../../../Shared/api";
 import { useDispatch } from "react-redux";
 import { signUpOperation } from "../../../Redux/auth/auth-operations";
-
-// import { Navigate } from "react-router-dom";
-// import useAuth from "../../../Shared/hooks/useAuth";
+import { Navigate } from "react-router-dom";
+import useAuth from "../../../Shared/hooks/useAuth";
 
 const RegisterPage = () => {
   const dispatch = useDispatch();
   const onSignUp = (data) => {
     dispatch(signUpOperation(data));
   };
-  // const isLogin = useAuth();
-  // if (isLogin) {
-  //   return <Navigate to="/home" />;
-  // }
+  const isLogin = useAuth();
+  if (isLogin) {
+    return <Navigate to="/home" />;
+  }
   return (
     <div className="container">
       <RegisterForm onSubmit={onSignUp} />
