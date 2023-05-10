0f558047c3156faec1aad1643902aeb076e55847
yuriipol <104125040+yuriipol@users.noreply.github.com>
2023-05-10 09:44:07 +0300

    add modal window and findInfo

---
+++ src/Component/FormChangeStatInfo/FormChangeStatInfo.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+import s from "./FormChangeStatInfo.module.scss";
+
+const FormChangeStatInfo = ({ onSubmit }) => {
+  const comands = [
+    "choose a team...",
+    "Polupan & Pavlov",
+    "Fesenko & Belik",
+    "Novikov & Garyachov",
+  ];
+  const places = ["choose a place...", "1 place", "2 place", "3 place"];
+  const [comand, setComand] = useState("");
+  const [placeOfChamp, setPlaceOfChamp] = useState("");
+  const [placeOfCup, setPlaceOfCup] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+
+  const hendleSubmit = (event) => {
+    event.preventDefault();
+    const data = {
+      date: startDate.toDateString(),
+      comand,
+      placeOfChamp: placeOfChamp,
+      placeOfCup: placeOfCup,
+    };
+    console.log(data);
+    onSubmit(data);
+
+    resetForm();
+  };
+
+  const resetForm = () => {
+    setComand("");
+    setPlaceOfChamp("");
+    setPlaceOfCup("");
+  };
+
+  const optionsComands = comands.map((comand, index) => {
+    return (
+      <option key={index} value={comand}>
+        {comand}
+      </option>
+    );
+  });
+  const optionsPlaces = places.map((place, index) => {
+    return (
+      <option key={index} value={place}>
+        {place}
+      </option>
+    );
+  });
+  return (
+    <div className="container">
+      <form onSubmit={hendleSubmit} className={s.form}>
+        <div className={s.addForm}>
+          <DatePicker
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            dateFormat="dd.MM.yyyy"
+            className={s.datePicker}
+          />
+          <select
+            value={comand}
+            onChange={(event) => setComand(event.target.value)}
+            className={s.select}
+          >
+            {optionsComands}
+          </select>
+          <select
+            value={placeOfChamp}
+            onChange={(event) => setPlaceOfChamp(event.target.value)}
+            className={s.select}
+          >
+            {optionsPlaces}
+          </select>
+          <select
+            value={placeOfCup}
+            onChange={(event) => setPlaceOfCup(event.target.value)}
+            className={s.select}
+          >
+            {optionsPlaces}
+          </select>
+        </div>
+        <button type="submit" className={s.addStat}>
+          Change info
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default FormChangeStatInfo;
--- src/Pages/StatisticsPage/StatisticsPage.jsx
+++ src/Pages/StatisticsPage/StatisticsPage.jsx
@@ -2,12 +2,17 @@ import { onSnapshot, collection } from "firebase/firestore";
 import { db } from "../../Shared/firebase/config";
 import FormAddWinners from "../../Component/FormAddWinners/FormAddWinners";
 import StatisticList from "../../Component/StatisticList/StatisticList";
+import Modal from "../../Component/Modal/Modal";
+import FormChangeStatInfo from "../../Component/FormChangeStatInfo/FormChangeStatInfo";
 import { uploadInfo } from "../../Shared/apiDB";
 import { useEffect, useState } from "react";
+import s from "./StatisticsPage.module.scss";
 
 const StatisticsPage = () => {
   const [info, setInfo] = useState([]);
   const [searchInfo, setSearchInfo] = useState({});
+  const [showModal, setShowModal] = useState(false);
+
   const hendleSubmit = (data) => {
     uploadInfo(data);
   };
@@ -27,17 +32,44 @@ const StatisticsPage = () => {
     return () => unScrible();
   }, []);
   // console.log(info);
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
 
   const findIdContact = (event) => {
-    const searchInfo = info.find((item) => item.id === event.currentTarget.id);
-    setSearchInfo(searchInfo);
+    const findInfo = info.find((item) => item.id === event.currentTarget.id);
+    setSearchInfo(findInfo);
     // console.log(searchInfo);
-    // toggleModal();
+    toggleModal();
   };
   return (
     <div className="container">
       <FormAddWinners onSubmit={hendleSubmit} />
       <StatisticList info={info} onClick={findIdContact} />
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <h2 className={s.title__change}>CHANGE STATISTIC INFO</h2>
+          <p className={s.champinship}>{searchInfo.date}</p>
+          <div className={s.findInfo}>
+            <p className={s.champinship}>Comand name: {searchInfo.comand}</p>
+            <p className={s.champinship}>
+              Place of Championship: {searchInfo.placeOfChamp}
+            </p>
+            <p className={s.champinship}>
+              Place of Cup: {searchInfo.placeOfCup}
+            </p>
+            {/* {(placeOfCup, placeOfChamp, date, comand, id)} */}
+          </div>
+          <button
+            type="button"
+            onClick={toggleModal}
+            className={s.modal__close}
+          >
+            close
+          </button>
+          <FormChangeStatInfo onClose={toggleModal} findContact={searchInfo} />
+        </Modal>
+      )}
     </div>
   );
 };
